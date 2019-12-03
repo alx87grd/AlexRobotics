8d51190359660129cf0054105526b08a587ca0a7
amy-f <frappier.amelie@gmail.com>
2019-12-03 13:32:44 -0500

    Simulation with scaled car (Proto Car)

--- projects/value_iteration/car_trajectory_with_valueiteration.py
+++ projects/value_iteration/car_trajectory_with_valueiteration.py
@@ -23,14 +23,14 @@ sys  = vehicle.KinematicCarModelwithObstacles()
 # Planning
 
 # Set domain
-sys.x_ub = np.array([+35,+3.5,+3])
-sys.x_lb = np.array([-5,-2,-3])
+sys.x_ub = np.array([+35, +3, +3])
+sys.x_lb = np.array([-5, -2, -3])
 
-sys.u_ub = np.array( [+3,+1] )
-sys.u_lb = np.array( [-3,-1] )
+sys.u_ub = np.array([+3, +1])
+sys.u_lb = np.array([-3, -1])
 
 # Discrete world
-grid_sys = discretizer.GridDynamicSystem( sys , (51, 51, 21) , (3,3) , 0.1 )
+grid_sys = discretizer.GridDynamicSystem(sys, (51, 51, 21), (3, 3), 0.1)
 # Cost Function
 cf = costfunction.QuadraticCostFunction(
     q=np.ones(sys.n),
@@ -40,7 +40,7 @@ cf = costfunction.QuadraticCostFunction(
 
 cf.xbar = np.array( [30, 0, 0] ) # target
 cf.INF  = 1E8
-cf.EPS  = 0
+cf.EPS  = 0.00
 cf.R    = np.array([[0.1,0],[0,0]])
 
 # VI algo
@@ -49,24 +49,25 @@ vi = valueiteration.ValueIteration_ND( grid_sys , cf )
 
 vi.uselookuptable = True
 vi.initialize()
+
 #if load_data:
-vi.load_data('car_vi_7')
-vi.compute_steps(30, plot=True, maxJ=6000)
+vi.load_data('car_vi_racecar_proto_scale_8')
+# vi.compute_steps(100, plot=True, maxJ=100)
 #if save_data:
-#vi.save_data('car_vi')
+# vi.save_data('car_vi_racecar_proto_scale_8')
 
 vi.assign_interpol_controller()
 
-vi.plot_cost2go(maxJ=50000)
+vi.plot_cost2go(maxJ=100)
 vi.plot_policy(0)
 vi.plot_policy(1)
 
 cl_sys = controller.ClosedLoopSystem( sys , vi.ctl )
 #
 ## Simulation and animation
-x0   = [8, -2, 0]
-tf   = 10
+x0   = [0, 0, 0]
+tf   = 5
 
 sim = cl_sys.compute_trajectory(x0, tf, 10001, 'euler')
 cl_sys.get_plotter().plot(sim, 'xu')
-cl_sys.get_animator().animate_simulation(sim, save=False, file_name='car_7-5')
+cl_sys.get_animator().animate_simulation(sim, save=True, file_name='car')
--- pyro/dynamic/vehicle.py
+++ pyro/dynamic/vehicle.py
@@ -680,6 +680,8 @@ class KinematicCarModel( KinematicBicyleModel ):
     ############################
     def __init__(self):
         """ """
+
+        # TODO: make car with 1/10th scale
         
         # initialize standard params
         KinematicBicyleModel.__init__(self,)
@@ -886,11 +888,11 @@ class KinematicCarModelwithObstacles( KinematicCarModel ):
         
         # Labels
         self.name = 'Kinematic Bicyle Model with Obstacles'
-        
+
         self.obstacles = [
-                [ (-10,-1),(-5,1)],
-                [ (-4,2),(1,4)],
-                [ (12,-1),(17,1)]
+                [ (-10, -1),(-5, 1)],
+                [ (-4, 2),(1, 4)],
+                [ (12, -1),(17, 1)]
                 ]
         
     #############################
@@ -953,10 +955,298 @@ class KinematicCarModelwithObstacles( KinematicCarModel ):
             lines_pts.append( pts )
             
                 
-        return lines_pts   
+        return lines_pts
 
-        
-    
+    ##############################################################################
+
+
+#
+##############################################################################
+
+class KinematicProtoCarModel(KinematicBicyleModel):
+    """
+
+    Bicycle model with car graphical output
+
+    Equations of Motion
+    -------------------------
+    dx   = V cos ( phi )
+    dy   = V sin ( phi )
+    dphi = V/l tan ( beta )
+
+    """
+
+    ############################
+    def __init__(self):
+        """ """
+
+        # initialize standard params
+        KinematicBicyleModel.__init__(self, )
+
+        # Model param
+        self.width = 1.7 * 0.1
+        self.a = 1.7 * 0.1
+        self.b = 1.7 * 0.1
+        self.lenght = self.a + self.b
+        self.lenght_tire = 0.40 * 0.1
+        self.width_tire = 0.15 * 0.1
+
+        # Graphic output parameters
+        self.dynamic_domain = True
+        self.dynamic_range = self.lenght * 2
+
+    ###########################################################################
+    # For graphical output
+    ###########################################################################
+
+    ###########################################################################
+    def forward_kinematic_lines(self, q):
+        """
+        Compute points p = [x;y;z] positions given config q
+        ----------------------------------------------------
+        - points of interest for ploting
+
+        Outpus:
+        lines_pts = [] : a list of array (n_pts x 3) for each lines
+
+        """
+
+        lines_pts = []  # list of array (n_pts x 3) for each lines
+
+        ###########################
+        # Top line
+        ###########################
+
+        pts = np.zeros((2, 10))
+
+        pts[0, 0] = -10000
+        pts[0, 1] = self.width * 2.25
+        pts[1, 0] = 10000
+        pts[1, 1] = self.width * 2.25
+
+        lines_pts.append(pts)
+
+        ###########################
+        # middle line
+        ###########################
+
+        pts = np.zeros((2, 10))
+
+        pts[0, 0] = -10000
+        pts[0, 1] = self.width * 0.75
+        pts[1, 0] = 10000
+        pts[1, 1] = self.width * 0.75
+
+        lines_pts.append(pts)
+
+        ###########################
+        # bottom line
+        ###########################
+
+        pts = np.zeros((2, 10))
+
+        pts[0, 0] = -10000
+        pts[0, 1] = - self.width * 0.75
+        pts[1, 0] = 10000
+        pts[1, 1] = - self.width * 0.75
+
+        lines_pts.append(pts)
+
+        ###########################
+        # Car
+        ###########################
+
+        """
+        Here is how the car is drawn:
+
+        |---------lenght--------|
+        |-----a------|
+        *                       *   -
+            *               *       |
+          d2    *   CG  *    d3     |
+                    *              width
+          d1    *       *    d4     |
+           *                *       |
+        *                       *   -
+        """
+        # Distance of the four corners of the car from the mass center
+        d1 = np.sqrt(self.a ** 2 + (self.width / 2) ** 2)
+        d3 = np.sqrt((self.lenght - self.a) ** 2 + (self.width / 2) ** 2)
+
+        # Angles of the four lines of the car
+        theta1 = np.pi + np.arctan(self.width / 2 / self.a) + q[2]
+        theta2 = np.pi - np.arctan(self.width / 2 / self.a) + q[2]
+        theta3 = np.arctan(self.width / 2 / (self.lenght - self.a)) + q[2]
+        theta4 = 2 * np.pi - np.arctan(self.width / 2 / (self.lenght - self.a)) + q[2]
+
+        # Build first line of the car
+        pts = np.zeros((2, 10))
+        pts[0, 0], pts[0, 1], pts[1, 0], pts[1, 1] = self.draw_line(d1, d1, theta1, theta2, q[0], q[1])
+        lines_pts.append(pts)
+
+        # Build second line of the car
+        pts = np.zeros((2, 10))
+        pts[0, 0], pts[0, 1], pts[1, 0], pts[1, 1] = self.draw_line(d3, d3, theta3, theta4, q[0], q[1])
+        px3 = pts[0, 0]  # Points used to center the wheels
+        py3 = pts[0, 1]
+        px4 = pts[1, 0]
+        py4 = pts[1, 1]
+        lines_pts.append(pts)
+
+        # Build third line of the car
+        pts = np.zeros((2, 10))
+        pts[0, 0], pts[0, 1], pts[1, 0], pts[1, 1] = self.draw_line(d1, d3, theta2, theta3, q[0], q[1])
+        lines_pts.append(pts)
+
+        # Build third fourth of the car
+        pts = np.zeros((2, 10))
+        pts[0, 0], pts[0, 1], pts[1, 0], pts[1, 1] = self.draw_line(d1, d3, theta1, theta4, q[0], q[1])
+        lines_pts.append(pts)
+
+        ###########################
+        # Wheels
+        ###########################
+
+        # Distance of the four corners of a tire from the center
+        d = np.sqrt((self.lenght_tire / 2) ** 2 + (self.width_tire / 2) ** 2)
+
+        # Angles of the four lines of a tire
+        steer1 = np.pi + np.arctan(self.width / 2 / self.a) + q[3] + q[2]
+        steer2 = np.pi - np.arctan(self.width / 2 / self.a) + q[3] + q[2]
+        steer3 = np.arctan(self.width / 2 / (self.lenght - self.a)) + q[3] + q[2]
+        steer4 = 2 * np.pi - np.arctan(self.width / 2 / (self.lenght - self.a)) + q[3] + q[2]
+
+        # Build first line of the left tire
+        pts = np.zeros((2, 10))
+        pts[0, 0], pts[0, 1], pts[1, 0], pts[1, 1] = self.draw_line(d, d, steer1, steer2, px3, py3)
+        lines_pts.append(pts)
+
+        # Build second line of the left tire
+        pts = np.zeros((2, 10))
+        pts[0, 0], pts[0, 1], pts[1, 0], pts[1, 1] = self.draw_line(d, d, steer3, steer4, px3, py3)
+        lines_pts.append(pts)
+
+        # Build third line of the left tire
+        pts = np.zeros((2, 10))
+        pts[0, 0], pts[0, 1], pts[1, 0], pts[1, 1] = self.draw_line(d, d, steer2, steer3, px3, py3)
+        lines_pts.append(pts)
+
+        # Build fourth line of the left tire
+        pts = np.zeros((2, 10))
+        pts[0, 0], pts[0, 1], pts[1, 0], pts[1, 1] = self.draw_line(d, d, steer1, steer4, px3, py3)
+        lines_pts.append(pts)
+
+        # Build first line of the right tire
+        pts = np.zeros((2, 10))
+        pts[0, 0], pts[0, 1], pts[1, 0], pts[1, 1] = self.draw_line(d, d, steer1, steer2, px4, py4)
+        lines_pts.append(pts)
+
+        # Build second line of the right tire
+        pts = np.zeros((2, 10))
+        pts[0, 0], pts[0, 1], pts[1, 0], pts[1, 1] = self.draw_line(d, d, steer3, steer4, px4, py4)
+        lines_pts.append(pts)
+
+        # Build third line of the right tire
+        pts = np.zeros((2, 10))
+        pts[0, 0], pts[0, 1], pts[1, 0], pts[1, 1] = self.draw_line(d, d, steer2, steer3, px4, py4)
+        lines_pts.append(pts)
+
+        # Build first line of the right tire
+        pts = np.zeros((2, 10))
+        pts[0, 0], pts[0, 1], pts[1, 0], pts[1, 1] = self.draw_line(d, d, steer1, steer4, px4, py4)
+        lines_pts.append(pts)
+
+        return lines_pts
+
+    ##########################################################################
+    def draw_line(self, d1, d2, angle1, angle2, x, y):
+        x1 = x + d1 * np.cos(angle1)
+        y1 = y + d1 * np.sin(angle1)
+        x2 = x + d2 * np.cos(angle2)
+        y2 = y + d2 * np.sin(angle2)
+
+        return x1, y1, x2, y2
+
+
+##############################################################################
+class KinematicProtoCarModelwithObstacles(KinematicProtoCarModel):
+    """
+
+    """
+
+    ############################
+    def __init__(self):
+        """ """
+        # initialize standard params
+        KinematicCarModel.__init__(self)
+
+        # Labels
+        self.name = 'Kinematic Bicyle Model with Obstacles'
+
+        self.obstacles = [
+            [(-10 * 0.1, -1 * 0.1), (-5 * 0.1, 1 * 0.1)],
+            [(-4 * 0.1, 2 * 0.1), (1 * 0.1, 4 * 0.1)],
+            [(12 * 0.1, -1 * 0.1), (17 * 0.1, 1 * 0.1)]
+        ]
+
+    #############################
+    def isavalidstate(self, x):
+        """ check if x is in the state domain """
+        ans = False
+        for i in range(self.n):
+            ans = ans or (x[i] < self.x_lb[i])
+            ans = ans or (x[i] > self.x_ub[i])
+
+        for obs in self.obstacles:
+            on_obs = ((x[0] + self.lenght * 0.5 > obs[0][0]) and
+                      (x[1] + self.width * 0.5 > obs[0][1]) and
+                      (x[0] - self.lenght * 0.5 < obs[1][0]) and
+                      (x[1] - self.width * 0.5 < obs[1][1]))
+
+            ans = ans or on_obs
+
+        return not (ans)
+
+    ###########################################################################
+    def forward_kinematic_lines(self, q):
+        """
+        Compute points p = [x;y;z] positions given config q
+        ----------------------------------------------------
+        - points of interest for ploting
+
+        Outpus:
+        lines_pts = [] : a list of array (n_pts x 3) for each lines
+
+        """
+
+        lines_pts = KinematicCarModel.forward_kinematic_lines(self, q)
+
+        ###########################
+        # obstacles
+        ###########################
+
+        for obs in self.obstacles:
+            pts = np.zeros((5, 3))
+
+            pts[0, 0] = obs[0][0]
+            pts[0, 1] = obs[0][1]
+
+            pts[1, 0] = obs[0][0]
+            pts[1, 1] = obs[1][1]
+
+            pts[2, 0] = obs[1][0]
+            pts[2, 1] = obs[1][1]
+
+            pts[3, 0] = obs[1][0]
+            pts[3, 1] = obs[0][1]
+
+            pts[4, 0] = obs[0][0]
+            pts[4, 1] = obs[0][1]
+
+            lines_pts.append(pts)
+
+        return lines_pts
+
 '''
 #################################################################
 ##################          Main                         ########
